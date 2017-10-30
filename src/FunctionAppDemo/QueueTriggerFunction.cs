4f7ea3104c154831e6034456b284f3390d2a6592
Ramón Tomás <rtomas@plainconcepts.com>
2017-10-31 00:28:24 +0100

    Added azure functions ppt Created demo Azure functions

---
+++ src/FunctionAppDemo/QueueTriggerFunction.cs
@@ -0,0 +1,26 @@
+using System;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+
+namespace FunctionAppDemo
+{
+    public static class QueueTriggerFunction
+    {
+        [FunctionName("QueueTriggerFunction")]
+        public static void Run([QueueTrigger("myqueue-items", Connection = "")]CustomQueueMessage myQueueItem, DateTimeOffset insertionTime, out object documentdb, TraceWriter log)
+        {
+            log.Info($"C# Queue trigger function processed: {myQueueItem.Name} inserted at {insertionTime}");
+
+            // Save message data from queue to DocumentDB
+            documentdb = new
+            {
+                name = myQueueItem.Name
+            };
+        }
+
+        public class CustomQueueMessage
+        {
+            public string Name { get; set; }
+        }
+    }
+}
